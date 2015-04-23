d58c58de942ad6ed6b90813ecf4a3bb057932f89
Yukiteru Amano <yukiteru.amano@gmx.com>
2015-04-22 23:49:00 -0430

    Nuevo Git

---
+++ README.md
@@ -0,0 +1,33 @@
+BlockHosts
+==========
+
+Un pequeño script bash para crear archivos /etc/hosts personalizados
+
+Este script usa las listas de MVPS, MalwareDomains y Someonecares 
+para crear un archivo hosts personalizado que bloquea buena parte de los
+dominios maliciosos y de publicidad de Internet. El script lleva
+consigo un sencillo instalador que a su vez automatiza todo el proceso
+inicial de configuración y realiza la creación inicial del hosts, 
+haciendo un respaldo del archivos hosts original.
+
+Para instalar el archivo solo necesita hacer el siguiente procedimiento:
+
+git clone https://github.com/josemald/BlockHosts.git
+
+cd BlockHosts
+
+sudo ./INSTALL
+
+Con eso ya tendrá instalado el script.
+
+Para ejecutar solo deben usar el comando
+
+sudo blockhost.sh 
+
+O hacerlo por la direccion completa
+
+sudo /usr/local/bin/blockhosts.sh
+
+Para desinstalar solo hagan
+
+sudo ./UNINSTALL
---
+++ blockhosts.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+if [ "$(id -u)" == "0" ]; then
+	# Respaldamos el archivo original de /etc/hosts en /etc/hosts.bak
+	if [ ! -f /etc/hosts.bak ]; then
+		echo "Respaldo del /etc/hosts original"
+		cp /etc/hosts /etc/hosts.bak
+	fi
+
+	# Descargamos las listas MVPS y MalwareDomain. 
+	# Para agregar otras listas debe crear los comandos necesarios
+	echo "Descargando listas..."
+	cd /tmp
+	wget -q http://www.malwaredomainlist.com/hostslist/hosts.txt -O malwaredomain.txt
+	wget -q http://winhelp2002.mvps.org/hosts.txt -O mvps.txt
+	wget -q http://someonewhocares.org/hosts/hosts -O someone.txt
+
+	# Usamos sed para eliminar las lineas que no queremos
+	echo "Listas descargadas. Limpiando..."
+	sed -i "s/127.0.0.1/0.0.0.0/g" mvps.txt
+	sed -i '/#/d' mvps.txt
+	sed -i '/localhost/d' mvps.txt
+
+	sed -i "s/127.0.0.1/0.0.0.0/g" someone.txt
+	sed -i '/#/d' someone.txt
+	sed -i '/localhost/d' someone.txt
+	sed -i '/broadcasthost/d' someone.txt
+	sed -i '/local/d' someone.txt
+
+	sed -i "s/127.0.0.1/0.0.0.0/g" malwaredomain.txt
+	sed -i '/#/d' malwaredomain.txt
+	sed -i '/localhost/d' malwaredomain.txt
+
+	# Recreamos el archivo hosts
+	echo "Recreando archivo /etc/hosts"
+	cat /etc/hosts.bak > hosts
+	cat malwaredomain.txt someone.txt mvps.txt >> hosts
+
+	# Movemos el nuevo archivo a su posicion final
+	echo "Copiando nuevo archivo /etc/hosts."
+	mv -v hosts /etc/hosts
+
+	# Borramos las listas originales
+	echo "Borrando temporales..."
+	rm malwaredomain.txt mvps.txt someone.txt
+
+	# Proceso terminado
+	echo "Blockhost ha terminado. Archivo /etc/hosts creado. Saliendo."
+else
+	echo "Se necesitan permisos de root o usar sudo para ejecutar este script"
+fi
+exit 0
